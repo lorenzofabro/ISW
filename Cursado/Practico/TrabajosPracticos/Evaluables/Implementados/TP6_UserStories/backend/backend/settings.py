2553f8d488e676e5467a4925b48f2f5335ffa75b
Lorenzo Fabro <lorenzofabro1997@gmail.com>
2020-09-01 19:59:18 -0300

    Modifico arcivo e instalo nuevos paquetes en frontend

--- Cursado/Practico/TrabajosPracticos/Evaluables/Implementados/TP6_UserStories/backend/backend/settings.py
+++ Cursado/Practico/TrabajosPracticos/Evaluables/Implementados/TP6_UserStories/backend/backend/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = (
+    'localhost:8080'
+)
 
 # Application definition
 
@@ -38,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'tp6_userstory'
+    'tp6_userstory',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'backend.urls'
--- Cursado/Practico/TrabajosPracticos/Evaluables/Implementados/TP6_UserStories/frontend/src/main.js
+++ Cursado/Practico/TrabajosPracticos/Evaluables/Implementados/TP6_UserStories/frontend/src/main.js
@@ -4,7 +4,10 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
 
-Vue.config.productionTip = false
+Vue.use(VueAxios, axios);
+axios.defaults.baseURL = 'http://localhost:8000/api';
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
